bdd4532a27461f126b1b3fe5cd8ff26389dcc912
Andy Park <andyp@opin.com>
2019-11-12 18:27:03 +0900

    Add documentation deps

--- mix.exs
+++ mix.exs
@@ -4,8 +4,12 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/atparkweb/first_elixir_project",
       elixir: "~> 1.9",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,7 +27,13 @@ defmodule Issues.MixProject do
     [
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       { :httpoison, "~> 1.6" },
-      { :poison,    "~> 4.0" }
+      { :poison,    "~> 4.0" },
+      { :ex_doc,    "~> 0.21.2" },
+      { :earmark,   "~> 1.4" }
     ]
+  end
+  
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
